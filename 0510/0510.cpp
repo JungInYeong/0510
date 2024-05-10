0de8646e99e3cb0131f663c5eaa898cf93921ee2
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-05-10 15:48:19 +0900

    dd

---
+++ 0510/0510.cpp
@@ -0,0 +1,37 @@
+﻿// 실습1 클래스 사용해보기
+
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+	double width, height;
+
+public:
+	Rectangle(double w, double h)
+	{
+		width = w;
+		height = h;
+	}
+	void area(double w, double h)
+	{
+		cout << "넓이는 : " << width * height << endl;
+	}
+};
+
+
+int main()
+{
+	double wid, hei;
+
+	cout << "사각형의 가로와 세로 길이를 입력해주세요. (띄어쓰기로 구분) : ";
+	cin >> wid >> hei;
+	Rectangle R(wid, hei);
+
+	R.area(wid, hei);
+
+
+
+
+	return 0;
+}
